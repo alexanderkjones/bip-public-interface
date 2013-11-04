611b7c9f6624b3c9e061809099d4f5cc3674d7c8
Anthony Mitchell <awmitchell@gmail.com>
2013-11-03 23:23:41 -0500

    left in http… whoops

--- config/initializers/neography.rb
+++ config/initializers/neography.rb
@@ -1,6 +1,6 @@
 Neography.configure do |config|
   config.protocol         = "http://"
-  config.server           = "http://6f3117f12.hosted.neo4j.org" #"localhost"
+  config.server           = "6f3117f12.hosted.neo4j.org" #"localhost"
   config.port             = 7570 #7474
   config.directory        = ""
   config.cypher_path      = "/cypher"
