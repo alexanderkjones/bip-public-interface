e5d85b3877e5b07254c873d73425201595fe0f38
Anthony Mitchell <awmitchell@gmail.com>
2013-11-11 20:16:02 -0500

    added bars, added confidence interval to bar labels

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,17 +16,15 @@ class HomeController < ApplicationController
 
   def results
     @neo = Neography::Rest.new
+    
+    products = params[:products].reject! { |c| c.empty? }
+    months = params[:months].reject! { |c| c.empty? }
 
-    products = []
-    months = []
-    params[:plans].each do |k, v|
-      v.each do |l, w|
-        if l != "" && w != ""
-          products.push(l)
-          months.push(w)
-        end
-      end
-    end
+    products.map! { |element| (element.downcase == "checkmite+") ? "checkmite" : element }
+
+    ############################
+    ## THIS IS OUR MAIN QUERY ##
+    ############################
 
     query_start = "START "
     (0..products.length-1).each do |t|
@@ -73,15 +71,103 @@ class HomeController < ApplicationController
       @ci_high = (ratio + (1.96 * (Math.sqrt(ratio) * (1.0 - ratio)) / @loss[0].to_f)) * 100
       @ci_low = (ratio - (1.96 * (Math.sqrt(ratio) * (1.0 - ratio)) / @loss[0].to_f)) * 100
       
-      @confidence_interval = ((@ci_high + @ci_low) / 2.0).to_i
+      @confidence_interval = ((@ci_high - @ci_low) / 2.0).round(2)
       
       @results = true
     else
       @results = false
     end
+
+    ####################################################
+    ## THIS IS OUR QUERY MATCHING SELECTED PRODUCT(S) ##
+    ####################################################
+    
+    query_start = "START "
+    (0..products.length-1).each do |t|
+      unless query_start.include?("#{products[t].downcase}=node:Products('name:#{products[t]}'), ")
+        query_start += "#{products[t].downcase}=node:Products('name:#{products[t]}'), "
+      end
+    end
+    query_start = query_start[0..-3]
+    
+    query_match = "MATCH "
+    (0..products.length-1).each do |t|
+      unless query_match.include?("(schedule) - [:APPLIED] -> (t) - [:PRODUCT_USED] -> (#{products[t].downcase}), ")
+        query_match += "(schedule) - [:APPLIED] -> (t) - [:PRODUCT_USED] -> (#{products[t].downcase}), "
+      end
+    end
+    query_match = query_match[0..-3]
+    
+    query_where = "WHERE (schedule.num_products = #{products.uniq.count}) "
+    query_with_distinct = "WITH DISTINCT schedule "
+    query_match2 = "MATCH (hiveloss) <- [:REPORTED] - (user) - [:USED] -> (schedule) "
+    query_return = "RETURN COUNT(user) as num_users, SUM(hiveloss.value) as sum_loss"
+
+    query = query_start + query_match + query_where + query_with_distinct + query_match2 + query_return
+    
+    puts ""
+    puts ""
+    puts ""
+    puts ""
+    puts query
+
+    @products_only = @neo.execute_query(query)["data"].first
+
+    ratio = (@products_only[1].to_f / @products_only[0].to_f).to_f * 0.01
+    
+    @po_ci_high = (ratio + (1.96 * (Math.sqrt(ratio) * (1.0 - ratio)) / @products_only[0].to_f)) * 100
+    @po_ci_low = (ratio - (1.96 * (Math.sqrt(ratio) * (1.0 - ratio)) / @products_only[0].to_f)) * 100
+    
+    @po_confidence_interval = ((@po_ci_high - @po_ci_low) / 2.0).round(2)
+    
+
+    ##################################################
+    ## THIS IS OUR QUERY MATCHING SELECTED MONTH(S) ##
+    ##################################################
+
+    query_start = "START "
+    (0..months.length-1).each do |t|
+      unless query_start.include?("#{months[t].downcase}=node:Months('name:#{months[t]}'), ")
+        query_start += "#{months[t].downcase}=node:Months('name:#{months[t]}'), "
+      end
+    end
+    query_start = query_start[0..-3]
+
+    query_match = "MATCH "
+    (0..months.length-1).each do |t|
+      unless query_match.include?("(schedule) - [:APPLIED] -> (t#{t+1}) - [:DATE_APPLIED] -> (#{months[t].downcase}), ")
+        query_match += "(schedule) - [:APPLIED] -> (t#{t+1}) - [:DATE_APPLIED] -> (#{months[t].downcase}), "
+      end
+    end
+    query_match = query_match[0..-3]
+
+    query_where = "WHERE (schedule.num_treatments = #{months.uniq.count}) "
+    query_with_distinct = "WITH DISTINCT schedule "
+    query_match2 = "MATCH (hiveloss) <- [:REPORTED] - (user) - [:USED] -> (schedule) "
+    query_return = "RETURN COUNT(user) as num_users, SUM(hiveloss.value) as sum_loss"
+
+    query = query_start + query_match + query_where + query_with_distinct + query_match2 + query_return
+    
+    puts ""
+    puts ""
+    puts ""
+    puts ""
+    puts query
+
+    @months_only = @neo.execute_query(query)["data"]
+
+    @months_only = @neo.execute_query(query)["data"].first
+
+    ratio = (@months_only[1].to_f / @months_only[0].to_f).to_f * 0.01
+    
+    @mo_ci_high = (ratio + (1.96 * (Math.sqrt(ratio) * (1.0 - ratio)) / @months_only[0].to_f)) * 100
+    @mo_ci_low = (ratio - (1.96 * (Math.sqrt(ratio) * (1.0 - ratio)) / @months_only[0].to_f)) * 100
+    
+    @mo_confidence_interval = ((@mo_ci_high - @mo_ci_low) / 2.0).round(2)
     
     respond_to do |format|
-      format.js { render partial: "results"}
+      format.html { render "results" }
+#      format.js { render partial: "results"}
 #      format.json { render json: {}, status: :ok }
     end
   end
